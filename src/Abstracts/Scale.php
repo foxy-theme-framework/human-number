86faed24dbece3f7bf113ce18dba94180fa09d3c
Puleeno Nguyen <puleeno@gmail.com>
2021-03-02 19:19:10 +0700

    Clean scale abstract

--- src/Abstracts/Scale.php
+++ src/Abstracts/Scale.php
@@ -7,9 +7,12 @@ abstract class Scale implements ScaleInterface
 {
     protected $decimals = 2;
     protected $unit = '';
-    protected $decimal_separator;
-    protected $thousands_separator;
 
+    protected $decimal_separator   = ',';
+    protected $thousands_separator = '.';
+
+    protected $jump  = 1000;
+    protected $units = array();
     protected $steps = array();
 
     public function __construct($init = array())
@@ -62,60 +65,7 @@ abstract class Scale implements ScaleInterface
         return $this->thousands_separator;
     }
 
-    public function _parse()
+    public static function create()
     {
-        $this->scale->sortSteps();
-        $allSteps = $this->scale->getSteps();
-        $stepKeys = array_keys($allSteps);
-        $totalStep = count($stepKeys);
-
-        foreach ($stepKeys as $index => $step) {
-            if ($this->parsed) {
-                break;
-            }
-            $cal = $this->raw / $step;
-
-            if ($cal >= 1) {
-                $this->value = $cal;
-                $this->prefix = $allSteps[$step];
-                if ($index >= $totalStep - 1) {
-                    $this->parsed = true;
-                }
-            } else {
-                $this->parsed = true;
-            }
-        }
-        if (!$this->parsed) {
-            if ($this->value <= 0) {
-                $this->value = $this->raw;
-            }
-
-            $this->parsed = true;
-        }
-    }
-
-    public function toArray()
-    {
-        if (!$this->parsed) {
-            $this->_parse();
-        }
-
-        $roundedPrice = number_format(
-            $this->value,
-            $this->scale->getDecimals(),
-            $this->scale->getDecimalSeparator(),
-            $this->scale->getThousandsSeparator()
-        );
-        return array(
-            'value'  => preg_replace('/\.0{1,}$/', '', $roundedPrice),
-            'prefix' => $this->prefix
-        );
-    }
-
-    public static function parse($number, $scale, $locale)
-    {
-        $p = new static($number, $scale, $locale);
-        $p->_parse();
-        return $p;
     }
 }
